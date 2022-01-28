43eb80203df22e126878eead09ae1c5c08b3345d
YupConislla <yup.conislla@nexus-virtual.net>
2022-01-27 23:25:27 -0500

    add actualization

--- encriptador/script.js
+++ encriptador/script.js
@@ -1,61 +1,49 @@
 
-/* Reglas de encriptación: 
-"e" es convertido para "enter" 
-"i" es convertido para "imes"
-"a" es convertido para "ai"
-"o" es convertido para "ober"
-"u" es convertido para "ufat"
-Solo letras minusculas
-No se permite acentuación de palabras 
-*/
 
-/* Reglas de desencriptación: 
-"enter" es convertido para "e" 
-"imes" es convertido para "i"
-"ai" es convertido para "a"
-"ober" es convertido para "o"
-"ufat" es convertido para "u"
-Solo letras minusculas
-No se permite acentuación de palabras   
-*/
+// const clear = document.getElementById('btnClear');
+// clear.addEventListener('click', limpiar)
 
 
-const encriptar = document.querySelector(#btnEncriptar);
-const desencriptar = document.querySelector(#btnDesencriptar).value;
-const text = document.querySelector(#inputTexto).value;
-const result = document.querySelector(#msg);
-const clear = document.querySelector(#btnClear);
-const copy = document.querySelector(#btnCopy);
 
-
-clear.addEventListener('click', limpiar);
-copy.addEventListener('click', copiar);
+const encriptar = document.getElementById('btnEncriptar');
+function encriptado() {
+    let result = document.getElementById('outputTexto');
+    let text = document.getElementById('inputTexto').value;
+    text = text.toLowerCase().trim();
+   
+    text = text.replaceAll("e","enter");
+    text = text.replaceAll("i","imes");
+    text = text.replaceAll("a","ai");
+    text = text.replaceAll("o","ober");
+    text = text.replaceAll("u","ufat");
+    result.value=text;
+    console.log(result);
+}
 encriptar.addEventListener('click', encriptado);
 
-function encriptado() {
-    text = text.replaceAll("e","enter");
-    text = text.replaceAll("i","imes");
-    text = text.replaceAll("a","ai");
-    text = text.replaceAll("o","ober");
-    text = text.replaceAll("u","ufat");
-    result.value= text
-}
 
-function desencriptado(){
-    text = text.replaceAll("e","enter");
-    text = text.replaceAll("i","imes");
-    text = text.replaceAll("a","ai");
-    text = text.replaceAll("o","ober");
-    text = text.replaceAll("u","ufat");
-    result.value= text
-}
+// const desencriptar = document.getElementById('btnDesencriptar');
+// desencriptar.addEventListener("click", function(e){
+//     e.preventDefault();
+//     const text = document.getElementById('inputTexto');
+//     const result = document.getElementById('outputTexto');
+//     result.value=desencriptado(text.value);
+// })
 
-function limpiar() {
-    text.value="";
-    result.value="";
-}
-function copiar() {
-    result.select();
-    document.execCommand('copy');
-    document.execCommand('copy');
-}
+
+
+
+// function limpiar() {
+//     text.value="";
+//     result.value="";
+// }
+
+// const copy = document.getElementById('btnCopy');
+// copy.addEventListener('click', function(copiar){
+//     copiar.preventDefault();
+//     const result = document.getElementById('outputTexto');
+//     console.log(result.value);
+//     result.select();
+//     navigator.clipboard.writeText(result.value)
+// });
+    
